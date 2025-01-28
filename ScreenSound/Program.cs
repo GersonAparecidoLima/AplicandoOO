d692c2c557be3bc255e976eed76d3de748529cf3
GersonAparecidoLima <gersonaparecido@gmail.com>
2025-01-28 15:12:33 -0300

    Property encapsular

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,26 @@
+﻿Musica musica1 = new Musica();
+musica1.Nome = "Faz Chover";
+musica1.Artista = "Fernando";
+musica1.Duracao = 273;
+musica1.Disponivel =  true;
+Console.WriteLine(musica1.DescricaoResumida);
+
+
+
+
+Musica musica2 = new Musica();
+musica2.Nome = "Com muito louvor";
+musica2.Artista = "Casiane";
+musica2.Duracao = 367;
+musica2.Disponivel = false;
+Console.WriteLine(musica2.DescricaoResumida);
+
+/*
+Console.WriteLine($"Nome da música : {musica1.nome}");
+Console.WriteLine($"Nome da música : {musica1.artista}");
+Console.WriteLine($"Nome da música : {musica2.nome}");
+Console.WriteLine($"Nome da música : {musica2.artista}");
+*/
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
